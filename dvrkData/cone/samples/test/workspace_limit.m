2e5733550ebddc16c59147d5f7d5b430f8b4b287
James <jamesyubai.di@gmail.com>
2020-10-08 10:30:43 -0700

    Add limit to workspace.

---
+++ dvrkData/cone/samples/test/workspace_limit.m
@@ -0,0 +1,11 @@
+% Workspace limit
+start_pos_psm1 = [-1.0826, -0.3033, -1.309];
+start_pos_psm2 = [-1.084, 0.3043, 1.128];
+
+
+xmax_psm1 = start_pos_psm1 + [0.3, 0.3, 0.3];
+xmin_psm1 = start_pos_psm1 - [0.3, 0.3, 0.3];
+
+xmax_psm2 = start_pos_psm2 + [0.3, 0.3, 0.3];
+xmin_psm2 = start_pos_psm2 - [0.3, 0.3, 0.3];
+
